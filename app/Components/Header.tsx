05453f625053379b8b7f77cbcdd771290b625af4
Arjun Venat <arjunvenat@gmail.com>
2024-05-06 14:12:54 -0400

    used TailwindCSS template to get some buttons there

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,34 +1,30 @@
-import React from 'react'
-import Image from "next/image";
-import { FaHome, FaInfo, FaBriefcase } from 'react-icons/fa';
-
+import React from 'react';
 export default function Header() {
-  return (
-    <header className="sticky top-0 z-50 w-full backdrop-blur flex-none bg-white flex justify-between items-center px-6 h-20 shadow-md">
-      <div className="max-w-8xl mx-auto">
-        <div className="flex justify-between items-center space-x-20 h-full">
-          <div className="flex items-center space-x-2">
-            <FaHome className="text-blue-500" />
-            <a href="/home" className="hover:text-blue-500">Home</a>
-          </div>
-          <div className="flex items-center space-x-2">
-            <FaInfo className="text-blue-500" />
-            <a href="/raw-upload" className="hover:text-blue-500">Upload Raw Data</a>
-          </div>
-          <div className="flex items-center space-x-2">
-            <FaBriefcase className="text-blue-500" />
-            <a href="/clean-upload" className="hover:text-blue-500">Upload Clean Data</a>
-          </div>
-          <div className="flex items-center space-x-2">
-            <FaBriefcase className="text-blue-500" />
-            <a href="/params" className="hover:text-blue-500">Set Parameters</a>
-          </div>
-          <div className="flex items-center space-x-2">
-            <FaBriefcase className="text-blue-500" />
-            <a href="/solver" className="hover:text-blue-500">Solver</a>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+
+    return (
+        <header className="sticky top-0 z-50 w-full backdrop-blur flex-none bg-white flex justify-between items-center px-6 h-20 shadow-md">
+            <div className="max-w-8xl mx-auto">
+                <div className="py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
+                    <div className="relative flex items-center">
+                        <div className="relative hidden lg:flex items-center ml-auto">
+                            <nav className="text-sm leading-6 font-semibold text-slate-700 dark:text-slate-200">
+                                <ul className="flex space-x-8">
+                                    <li><a className="hover:text-sky-500 dark:hover:text-sky-400"
+                                           href="/docs/installation">Docs</a></li>
+                                    <li><a href="https://tailwindui.com/?ref=top"
+                                           className="hover:text-sky-500 dark:hover:text-sky-400">Components</a></li>
+                                    <li><a className="hover:text-sky-500 dark:hover:text-sky-400" href="/blog">Blog</a>
+                                    </li>
+                                    <li><a className="hover:text-sky-500 dark:hover:text-sky-400"
+                                           href="/showcase">Showcase</a></li>
+                                    <li><a className="hover:text-sky-500 dark:hover:text-sky-400"
+                                           href="/showcase">Showcase</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 }
