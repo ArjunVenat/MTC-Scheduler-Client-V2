8b8caa483fe026f7682f21cb5072ac61e255c142
Arjun Venat <arjunvenat@gmail.com>
2024-05-11 22:32:23 -0400

    table template imported and slightly adapted

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../Components/Header';
-import LargeTable from '../Components/LargeTable';
+import WorkersTable from '../Components/Table';
 import HeroPage from "@/app/Components/HeroPage";
 
 export default function Home() {
